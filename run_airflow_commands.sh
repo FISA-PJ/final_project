28304e61bd7189934e93bae8e0d21ff4541fa9fa
taeminlee25 <ltm7149@konkuk.ac.kr>
2025-05-21 09:29:49 +0900

    fix: airflow 서비스 시작 셀 스크립트 수정

--- run_airflow_commands.sh
+++ run_airflow_commands.sh
@@ -9,7 +9,7 @@ docker compose up airflow-init
 
 echo "[INFO] airflow-init completed (first-time run). Starting other services..."
 
-# airflow-init 이후 다른 모든 서비스 실행
+# 다른 airflow-* 서비스 실행
 docker compose up airflow-* -d
 
 echo "[INFO] All airflow services are now running!"
